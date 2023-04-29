b3743f96a1a4a9b61fb17f5da5f0535313ca8bd6
pyl <a522723936@gmail.com>
2023-04-29 22:52:52 +0800

    add validation_error_handler

--- chatgpt_wrapper/blueprints/error_handlers.py
+++ chatgpt_wrapper/blueprints/error_handlers.py
@@ -1,5 +1,9 @@
 from flask import jsonify
+from flask_inputs import Inputs
 
 
 def default_error_handler(message, status_code=500):
     return jsonify({"success": False, "error": str(message)}), status_code
+
+def validation_error_handler(inputs: Inputs, status_code=422):
+    return jsonify({'message': 'invalid input data', 'errors': inputs.errors}), status_code
