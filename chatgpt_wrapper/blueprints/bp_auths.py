bf4d0c5f1cf49ececa55db2ec192bb4f812aefc7
pyl <a522723936@gmail.com>
2023-05-03 12:09:11 +0800

    add logout

--- chatgpt_wrapper/blueprints/bp_auths.py
+++ chatgpt_wrapper/blueprints/bp_auths.py
@@ -1,6 +1,6 @@
 # app/auth/__init__.py
 from flask import Blueprint, current_app, jsonify, request
-from flask_login import LoginManager, current_user, login_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_inputs import Inputs
 from flask_inputs.validators import JsonSchema
 from chatgpt_wrapper.backends.openai.api import OpenAIAPI
@@ -45,4 +45,8 @@ def login():
     else:
         return jsonify({'success': False, 'reason': msg}), 401
 
-
+@auth_bp.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'You have been logged out successfully.'}), 200
