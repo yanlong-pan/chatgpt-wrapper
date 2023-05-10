07b47467716558dbb25565239892f4d67675f99d
pyl <a522723936@gmail.com>
2023-05-10 21:03:59 +0800

    update log path and format

--- chatgpt_wrapper/core/logger.py
+++ chatgpt_wrapper/core/logger.py
@@ -1,12 +1,15 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
+import os
 
 def setup_logging():
     # 根据需要自定义日志格式
-    log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log_format = logging.Formatter('%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
 
+    log_path = "/logs"
+    os.makedirs(log_path, exist_ok=True)
     # 创建一个定时循环文件处理器，每天创建一个新文件
-    file_handler = TimedRotatingFileHandler("/logs/app", when='D', interval=1)
+    file_handler = TimedRotatingFileHandler(f'{log_path}/app.log', when='D', interval=1)
     file_handler.setFormatter(log_format)
     
     # 创建一个新的日志记录器
