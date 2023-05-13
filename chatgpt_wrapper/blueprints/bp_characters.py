891de530e8c798cdb12fbae320f46d96c2cfd15c
pyl <a522723936@gmail.com>
2023-05-13 12:45:36 +0800

    concatenate url for avatar and voice

--- chatgpt_wrapper/blueprints/bp_characters.py
+++ chatgpt_wrapper/blueprints/bp_characters.py
@@ -1,14 +1,24 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 
 from chatgpt_wrapper.utils.cache import get_cached_characters
 
 characters_bp = Blueprint('characters', __name__, url_prefix='/api/v1/characters')
 
+def concat_resource_url(path):
+    return request.host_url + path if path else ''
+
 @characters_bp.route('/', methods=['GET'])
 @login_required
 def list_characters():
-    return jsonify(get_cached_characters()), 201
+    return jsonify([
+        {key: {
+            'avatar': concat_resource_url(value.get('avatar')),
+            'voice': concat_resource_url(value.get('voice')),
+        }} 
+        for item in get_cached_characters() 
+        for key, value in item.items() 
+    ]), 201
    
 
     
